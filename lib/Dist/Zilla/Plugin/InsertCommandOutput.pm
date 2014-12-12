f1ab70025459cb778f005b88c203b1392545320f
perlancar <perlancar@gmail.com>
2014-12-12 05:03:21 +0000

    Import of PERLANCAR/Dist-Zilla-Plugin-InsertCommandOutput-0.02.tar.gz from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-InsertCommandOutput
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PERLANCAR/Dist-Zilla-Plugin-InsertCommandOutput-0.02.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/InsertCommandOutput.pm
+++ lib/Dist/Zilla/Plugin/InsertCommandOutput.pm
@@ -1,7 +1,7 @@
 package Dist::Zilla::Plugin::InsertCommandOutput;
 
 our $DATE = '2014-12-12'; # DATE
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 use 5.010001;
 use strict;
@@ -63,7 +63,7 @@ Dist::Zilla::Plugin::InsertCommandOutput - Insert the output of command into you
 
 =head1 VERSION
 
-This document describes version 0.01 of Dist::Zilla::Plugin::InsertCommandOutput (from Perl distribution Dist-Zilla-Plugin-InsertCommandOutput), released on 2014-12-12.
+This document describes version 0.02 of Dist::Zilla::Plugin::InsertCommandOutput (from Perl distribution Dist-Zilla-Plugin-InsertCommandOutput), released on 2014-12-12.
 
 =head1 SYNOPSIS
 
